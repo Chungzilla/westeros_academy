2d5f34b1eed99b8b1f489225b3990eb6d320a4a7
Chungzilla <chung.jazmine@gmail.com>
2018-08-22 17:52:48 -0400

    genereated user model

---
+++ db/migrate/20180822215153_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :gender
+      t.string :photo
+      t.string :username
+      t.string :email
+      t.string :password
+      t.references :userable, polymorphic: true
+
+      t.timestamps
+    end
+  end
+end
