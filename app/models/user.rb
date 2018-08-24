d857bcc7942421ed5dd6273d4dffd6d354257a0b
Chungzilla <chung.jazmine@gmail.com>
2018-08-24 18:38:42 -0400

    setup root

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   belongs_to :userable, polymorphic: true
-  has_one :house
+  belongs_to :house
   has_many :courses, :through => :enrollments
   validates :first_name, :last_name, :gender, :username, :email, :password, presence: true
   validates :email, :username, uniqueness: true
