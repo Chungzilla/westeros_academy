35f076e9bd5d24d47ddd987d13ef214a217cb9a5
Chungzilla <chung.jazmine@gmail.com>
2018-08-24 17:28:42 -0400

    changed controller file names

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
-cRails.application.routes.draw do
+Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  resources :user
-  resources :admin
-  resources :student
-  resources :instructor
-  resources :parent
-  resources :course
-  resources :house
+  root 'users#index'
+
+  resources :users
+  resources :admins
+  resources :students
+  resources :instructors
+  resources :parents
+  resources :courses
+  resources :houses
+  resources :enrollments
 
 end
 
